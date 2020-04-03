458fa459f36f207da0d6f372a7b2c6a627236553
Quy <tranphuquy19@gmail.com>
2020-04-03 15:51:16 +0700

    clean code

--- buoi3/promiseThenable.js
+++ buoi3/promiseThenable.js
@@ -2,10 +2,8 @@ let myPromise = new Promise((resolve, reject) => {
     reject('Error!');
 });
 
-myPromise.then(result => {
-    console.log(result);
-}, error => {
-    console.log(error);
-}).catch(error => {
-    console.log(`error in catch: ${error}`);
-});
+myPromise
+    .then(
+        result => console.log(result),
+        error => console.log(error))
+    .catch(error => console.log(`error in catch: ${error}`));
